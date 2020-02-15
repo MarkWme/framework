eb95a9e3c41a9b54f401f1f0f297aa0ca6a50816
Mark Whitby <github@chipfat.com>
2020-02-15 10:44:22 +0000

    Removed tags from subnet resource

--- modules/core/main.tf
+++ modules/core/main.tf
@@ -41,17 +41,6 @@ resource "azurerm_subnet" "core-subnet" {
   resource_group_name = azurerm_resource_group.core-resource-group.name
   virtual_network_name = azurerm_virtual_network.core-virtual-network.name
   address_prefix = "10.0.0.0/26"
-  tags = {
-    deployed-by = "terraform"
-    timestamp = timestamp()
-    description = "Subnet for Azure Firewall"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      tags["timestamp"],
-    ]
-  }
 }
 
 resource "azurerm_public_ip" "core-firewall-pip" {
