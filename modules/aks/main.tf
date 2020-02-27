523518b445182dfcb6bffa899cdd5069dc38a259
Mark Whitby <github@chipfat.com>
2020-02-27 22:33:00 +0000

    Initial attempt at adding kured support

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,3 +1,10 @@
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+}
+
 data "azurerm_key_vault_secret" "ssh_key" {
   name      = var.ssh_key_name
   key_vault_id = var.key_vault_id
@@ -54,7 +61,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   default_node_pool {
     name = "linux01"
-    vm_size = "Standard_DS2_v2"
+    vm_size = "Standard_D2s_v3"
     type = "VirtualMachineScaleSets"
     enable_auto_scaling = var.enable_auto_scaling
     min_count = var.minimum_node_count
@@ -106,7 +113,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "aks_windows" {
   count                 = var.enable_windows_containers ? 1:0
   name                  = "win01"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size               = "Standard_DS2_v2"
+  vm_size               = "Standard_D2s_v3"
   enable_auto_scaling = var.enable_auto_scaling
   min_count = var.minimum_node_count
   max_count = var.maximum_node_count
@@ -126,4 +133,178 @@ resource "azurerm_key_vault_secret" "windows_admin_password" {
   name         = format("%s-windows-admin-password", azurerm_kubernetes_cluster.aks.name)
   value        = random_password.windows_admin_password.result
   key_vault_id = var.key_vault_id
+}
+
+#
+# kured
+#
+
+# Based on the weaveworks/kured template here: https://github.com/weaveworks/kured/releases/download/1.2.0/kured-1.2.0-dockerhub.yaml
+# More info: https://github.com/weaveworks/kured
+
+resource "kubernetes_service_account" "kured_service_account" {
+  provider = kubernetes
+  automount_service_account_token = true
+  metadata {
+    name      = "kured"
+    namespace = "kube-system"
+  }
+}
+
+resource "kubernetes_role" "kured_role" {
+  provider = kubernetes
+  metadata {
+    name      = "kured"
+    namespace = "kube-system"
+  }
+  rule { # Allow kured to lock/unlock itself
+    api_groups     = ["apps"]
+    resources      = ["daemonsets"]
+    resource_names = ["kured"]
+    verbs          = ["update", "get"]
+  }
+}
+
+resource "kubernetes_cluster_role" "kured_cluster_role" {
+  provider = kubernetes
+  metadata {
+    name = "kured"
+  }
+
+  # #
+  # Allow kured to read spec.unschedulable
+  # Allow kubectl to drain/uncordon
+  # #
+  rule {
+    api_groups = [""]
+    resources  = ["nodes"]
+    verbs      = ["get", "patch"]
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["pods"]
+    verbs      = ["list", "delete", "get"]
+  }
+  rule {
+    api_groups = ["apps"]
+    resources  = ["daemonsets"]
+    verbs      = ["get"]
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["pods/eviction"]
+    verbs      = ["create"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "kured_cluster_role_binding" {
+  provider = kubernetes
+  metadata {
+    name = "kured"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.kured_cluster_role.metadata[0].name
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.kured_service_account.metadata[0].name
+    namespace = kubernetes_service_account.kured_service_account.metadata[0].namespace
+  }
+}
+
+resource "kubernetes_role_binding" "kured_role_binding" {
+  provider = kubernetes
+  metadata {
+    name      = "kured"
+    namespace = "kube-system"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role.kured_role.metadata[0].name
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.kured_service_account.metadata[0].name
+    namespace = kubernetes_service_account.kured_service_account.metadata[0].namespace
+  }
+}
+
+resource "kubernetes_daemonset" "kured_daemonset" {
+  provider = kubernetes
+  metadata {
+    name      = "kured"
+    namespace = "kube-system"
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        name = "kured"
+      }
+    }
+    strategy {
+      type = "RollingUpdate"
+    }
+    template {
+      metadata {
+        labels = {
+          name = "kured"
+        }
+      }
+      spec {
+        automount_service_account_token = true
+        service_account_name = kubernetes_service_account.kured_service_account.metadata[0].name
+        toleration {
+          key    = "node-role.kubernetes.io/master"
+          effect = "NoSchedule"
+        }
+        node_selector = {
+          "beta.kubernetes.io/os" = "linux"
+        }
+        host_pid       = true # Facilitate entering the host mount namespace via init
+        restart_policy = "Always"
+        container {
+          name              = "kured"
+          image             = "docker.io/weaveworks/kured:master-f6e4062"
+          image_pull_policy = "IfNotPresent"
+          security_context { # Give permission to nsenter /proc/1/ns/mnt
+            privileged = true
+          }
+          env {
+            # Pass in the name of the node on which this pod is scheduled
+            # for use with drain/uncordon operations and lock acquisition
+            name = "KURED_NODE_ID"
+            value_from {
+              field_ref {
+                field_path = "spec.nodeName"
+              }
+            }
+          }
+          command = ["/usr/bin/kured"]
+          args = [
+            "--alert-filter-regexp=^RebootRequired$",              # alert names to ignore when checking for active alerts
+            "--blocking-pod-selector=runtime=long,cost=expensive", # label selector identifying pods whose presence should prevent reboots
+            "--blocking-pod-selector=name=temperamental",
+            "--blocking-pod-selector=...",
+            "--ds-name=kured",                               # name of daemonset on which to place lock
+            "--ds-namespace=kube-system",                    # namespace containing daemonset on which to place lock
+            "--lock-annotation=weave.works/kured-node-lock", # annotation in which to record locking node
+            "--reboot-sentinel=/var/run/reboot-required",    # path to file whose existence signals need to reboot
+            "--time-zone=UTC",                               # use this timezone to calculate allowed reboot time
+            "--period=1h0m0s",                               # reboot check period (default 1h0m0s)
+            "--start-time=1am",          # only reboot after this time of day
+            "--end-time=11pm",              # only reboot before this time of day
+            #           additional configurable args
+            # #            - --prometheus-url=http://prometheus.monitoring.svc.cluster.local
+            # #            - --slack-hook-url=https://hooks.slack.com/...
+            # #            - --slack-username=prod
+            # #            - --slack-channel=xyz
+          ]
+        }
+      }
+    }
+  }
 }
--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,7 +1,3 @@
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
-}
-
 output "kube_config" {
   value = azurerm_kubernetes_cluster.aks.kube_config_raw
 }
@@ -12,4 +8,28 @@ output "subnet_id" {
 
 output "windows_admin_password" {
   value = random_password.windows_admin_password.result
+}
+
+output "host" {
+ value = azurerm_kubernetes_cluster.aks.kube_config.0.host
+} 
+
+output "username" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.username
+}
+
+output "password" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.password
+}
+
+output "client_certificate" {
+  value = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+}
+ 
+output "client_key" {
+  value = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+}
+
+output "cluster_ca_certificate" {
+  value = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
 }
